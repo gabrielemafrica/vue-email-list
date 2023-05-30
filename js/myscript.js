23b27b750c64fdccf820750ce38b00198605467c
gabrielemafrica <gabriele.ga@live.it>
2023-05-30 10:47:13 +0300

    v if posizionato bene

--- js/myscript.js
+++ js/myscript.js
@@ -4,24 +4,19 @@ createApp({
     data(){
         return {
             mails: [],
-
         }
     },
     mounted() {
-        
         // ciclo per generare 10 mail
-        for (let i = 0; i < 10; i++) {
-            
+        for (let i = 0; i < 10; i++) { 
             // chiamata axios
             axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then(
                 risposta => {
-
                     // pusho la mail nell'array
                     const mail = risposta.data.response;
-                    this.mails.push(mail);
-                   
+                    this.mails.push(mail);                   
             });
         } console.log(this.mails);   
     }
